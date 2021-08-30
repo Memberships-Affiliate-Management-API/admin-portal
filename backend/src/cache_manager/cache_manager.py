b85096d67b3ddb9f0bfd543f17a481e849f0f94c
mobius-crypt <mobiusndou@gmail.com>
2021-08-30 12:34:07 +0200

    added cache manager

--- backend/src/cache_manager/cache_manager.py
+++ backend/src/cache_manager/cache_manager.py
@@ -1,4 +1,29 @@
+"""
+    **cache Manager**
+        this module will handle all requests from this application to other api's
+
+"""
+__author__ = "mobius-crypt"
+__email__ = "mobiusndou@gmail.com"
+__twitter__ = "@blueitserver"
+__github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
+__github_profile__ = "https://github.com/freelancing-solutions/"
+
 from flask_caching import Cache
 from config import config_instance
 
-app_cache: Cache = Cache(config=config_instance.cache_dict())
+
+class CacheManager:
+
+    def __init__(self) -> None:
+        self.app_cache: Cache = Cache()
+
+    def init_app(self, app, config):
+        self.app_cache.init_app(app=app, config=config)
+
+    @property
+    def cache(self) -> Cache:
+        return self.app_cache
+
+
+cache_man = CacheManager()
