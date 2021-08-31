38dc57423813b797791e149caf511e7ab6e4985e
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 20:13:27 +0200

    updated: dom resources

---
+++ main/app/resources/static/js/dom_resources.js
@@ -0,0 +1,97 @@
+function return_dashboard_dom(){
+    return `
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Dashboard </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>           
+    `
+}
+
+function return_organizations_dom(){
+
+    return `
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Organizations </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+    `
+}
+
+function return_users_dom(){
+    return `
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Users </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+    
+    `
+}
+
+function return_api_dom(){
+   return `
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> API Keys </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+   
+   `
+}
+
+function return_affiliates_dom(){
+    return `
+        <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Affiliates </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+
+    `
+}
+
+function return_accounts_dom(){
+    return `
+        <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Accounts </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+
+    `
+}
+
+function return_help_desk_dom(){
+    return `
+        <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"> Help Desk </h3>               
+        </div>
+        <div class="card-body">
+        
+        </div>            
+    </div>    
+
+    `
+}
+
