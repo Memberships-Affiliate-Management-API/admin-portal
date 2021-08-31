2430855c06f7f0b51825768cc484ed98dff9401a
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:45:46 +0200

    code refactored docstrings

--- backend/src/admin_view/__init__.py
+++ backend/src/admin_view/__init__.py
@@ -1,3 +1,14 @@
+"""
+    **module admin view**
+        allows system admin to control how to access data from admin api
+"""
+__developer__ = "mobius-crypt"
+__email__ = "mobiusndou@gmail.com"
+__twitter__ = "@blueitserver"
+__github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
+__github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
+
 from backend.src.admin_requests.api_requests import app_requests
 from backend.src.utils import return_ttl
 from config import config_instance
@@ -5,8 +16,12 @@ from backend.src.cache_manager.cache_manager import cache_man
 
 
 class AdminView:
+    """
+    **Class AdminView**
+        logic needed to get system admin data
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._login_endpoint: str = '_api/v1/admin/auth/login'
         self._logout_endpoint: str = '_api/v1/admin/auth/logout'
         self._all_org_endpoint: str = '_api/v1/admin/organizations/get-all'
@@ -16,13 +31,13 @@ class AdminView:
         self._organization_id: str = config_instance.ORGANIZATION_ID
 
     @cache_man.cache.memoize(timeout=return_ttl('short'))
-    def login_user(self, email, password) -> dict:
+    def login_user(self, email: str, password: str) -> dict:
         """
         **login_user**
             will return login token
         """
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, email=email, password=password)
-        _request_id = app_requests.schedule_data_send(_endpoint=self._login_endpoint, body=_kwargs)
+        _request_id: str= app_requests.schedule_data_send(_endpoint=self._login_endpoint, body=_kwargs)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
@@ -34,7 +49,7 @@ class AdminView:
         **logout_user**
         """
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, email=email, token=token)
-        _request_id = app_requests.schedule_data_send(_endpoint=self._logout_endpoint, body=_kwargs)
+        _request_id: str = app_requests.schedule_data_send(_endpoint=self._logout_endpoint, body=_kwargs)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
@@ -47,7 +62,7 @@ class AdminView:
 
         """
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, domain=domain, token=token)
-        _request_id = app_requests.schedule_data_send(_endpoint=self._all_org_endpoint, body=_kwargs)
+        _request_id: str = app_requests.schedule_data_send(_endpoint=self._all_org_endpoint, body=_kwargs)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
@@ -65,7 +80,7 @@ class AdminView:
         """
 
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, domain=domain, token=token)
-        _request_id = app_requests.schedule_data_send(_endpoint=self._all_org_endpoint, body=_kwargs)
+        _request_id: str = app_requests.schedule_data_send(_endpoint=self._all_org_endpoint, body=_kwargs)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
@@ -82,7 +97,7 @@ class AdminView:
         return dict: system organization users or clients
         """
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, domain=domain, token=token)
-        _request_id = app_requests.schedule_data_send(_endpoint=self._all_api_keys, body=_kwargs)
+        _request_id: str = app_requests.schedule_data_send(_endpoint=self._all_api_keys, body=_kwargs)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
