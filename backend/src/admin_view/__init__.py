e16df5e876174d89a8022afb60b1ff110becbb03
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 10:09:01 +0200

    refactored authentication

--- backend/src/admin_view/__init__.py
+++ backend/src/admin_view/__init__.py
@@ -1,15 +1,19 @@
 from backend.src.admin_requests.api_requests import app_requests
+from backend.src.utils import return_ttl
 from config import config_instance
-from time import sleep
+from backend.src.cache_manager.cache_manager import cache_man
+
 
 class AdminView:
 
     def __init__(self):
         self._login_endpoint: str = '_api/v1/admin/auth/login'
+        self._logout_endpoint: str = '_api/v1/admin/auth/logout'
         self._uid: str = config_instance.ADMIN_UID
         self._email: str = config_instance.ADMIN_EMAIL
         self._organization_id: str = config_instance.ORGANIZATION_ID
 
+    @cache_man.cache.memoize(timeout=return_ttl('short'))
     def login_user(self, email, password) -> dict:
         """
         **login_user**
@@ -17,10 +21,16 @@ class AdminView:
         """
         _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, email=email, password=password)
         _request_id = app_requests.schedule_data_send(_endpoint=self._login_endpoint, body=_kwargs)
-        print('sending login request with id_: ', _request_id)
         while True:
             response = app_requests.get_response(request_id=_request_id)
             if response is not None:
                 return response
-            sleep(1)
+
+    @cache_man.cache.memoize(timeout=return_ttl('short'))
+    def logout_user(self, email) -> dict:
+        """
+        **logout_user**
+        """
+        _kwargs: dict = dict(uid=self._uid, organization_id=self._organization_id, email=email)
+        _request_id = app_requests.schedule_data_send(_endpoint=self._logout_endpoint, body=_kwargs)
 
