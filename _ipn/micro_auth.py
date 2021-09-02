5180e7d481047aca7b2fc444741623fab83a9647
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 23:44:20 +0200

    refactored users auth added caching

--- _ipn/micro_auth.py
+++ _ipn/micro_auth.py
@@ -13,15 +13,19 @@ import hmac
 from flask import Blueprint, jsonify, request
 
 from backend.src.custom_exceptions.exceptions import status_codes, UnAuthenticatedError
+from backend.src.utils import return_ttl, is_development
 from config import config_instance
-
+from backend.src.cache_manager.cache_manager import cache_man
 microservices_ipn_bp = Blueprint("microservices_ipn", __name__)
 
 
 @microservices_ipn_bp.route('/_ipn/micro-services/verify-app-id', methods=["POST"])
+@cache_man.cache.cached(timeout=return_ttl('short'), unless=is_development(), cache_none=False)
 def verify_app_id() -> tuple:
     """
-
+    **verify_app_id**
+        The API will try to validate the app_id, this application will only verify the app_id
+        if it can also verify the identity of the api server through its domain and also the secret key
     """
     json_data: dict = request.get_json()
     domain: str = json_data.get('domain')
@@ -39,6 +43,3 @@ def verify_app_id() -> tuple:
     if admin_domain_compare and secret_key_compare:
         return jsonify({'status': True, 'payload': _payload, 'message': 'app id verified'}), status_codes.status_ok_code
     raise UnAuthenticatedError()
-
-
-
