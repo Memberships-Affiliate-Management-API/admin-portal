f132da60b2fdf0309373f9c74de5af6e13a0034a
mobius-crypt <mobiusndou@gmail.com>
2021-09-18 10:07:41 +0200

    fixed arguments

--- backend/src/scheduler/scheduler.py
+++ backend/src/scheduler/scheduler.py
@@ -16,7 +16,7 @@ task_scheduler = Scheduler()
 # cron_scheduler = Scheduler()
 
 
-def create_task(func: Callable, job_name: str = "create_task", kwargs: Optional[dict] = None) -> tuple:
+def create_task(func: Callable, job_name: str, kwargs: Optional[dict] = None) -> Job:
     """
         **schedule_cache_deletion**
             schedule cache deletion such that it occurs sometime time in the future
@@ -31,47 +31,8 @@ def create_task(func: Callable, job_name: str = "create_task", kwargs: Optional[
     """
     _args: dict = dict()
     _job_names: Hashable = hash(job_name)
-    if kwargs:
+    if isinstance(kwargs, dict):
         print(f'creating job with kwargs: {kwargs}')
-        return task_scheduler.every(interval=30).seconds.do(job_func=func, kwargs=kwargs).tag(_job_names)
+        return task_scheduler.every(interval=30).seconds.do(func, **kwargs).tag(_job_names)
     return task_scheduler.every(interval=30).seconds.do(job_func=func).tag(_job_names)
 
-# def _create_task(func: Callable, kwargs: dict, delay: int = 10, job_name: str = "create_task") -> tuple:
-#     """
-#
-#     """
-#     global connection
-#     # noinspection PyUnresolvedReferences
-#     _body: bytes = json.dumps(dict(func=func.__name__, kwargs=kwargs, job_name=job_name)).encode(encoding='UTF-8')
-#
-#     try:
-#         connection = pika.BlockingConnection(pika.ConnectionParameters(host='amqps:memberships-rabbitmq'))
-#         channel = connection.channel()
-#         channel.queue_declare(queue='admin_task_queue', durable=True)
-#         channel.basic_publish(
-#             exchange='',
-#             routing_key='task_queue',
-#             body=_body,
-#             properties=pika.BasicProperties(
-#                 delivery_mode=2,  # make message persistent
-#             ))
-#
-#     except pika.exceptions.ConnectionClosedByBroker as e:
-#         print(f'Create Task Error: {e}')
-#         pass
-#     # Don't recover on channel errors
-#     except pika.exceptions.AMQPChannelError as e:
-#         print(f'Create Task Error: {e}')
-#         pass
-#     # Recover on all other connection errors
-#     except pika.exceptions.AMQPConnectionError as e:
-#         print(f'Create Task Error: {e}')
-#         pass
-#     except socket.gaierror as e:
-#         print(f'Create Task Error: {e}')
-#         pass
-#     finally:
-#         if not not connection:
-#             connection.close()
-#
-#     return _body, status_codes.status_ok_code
